cf772b526b1462ba271ab03b5713150db0798b76
NotSet <123@abc.com>
2021-06-18 20:39:59 +0700

    - Init project

---
+++ exporting-pdf/src/main/java/com/tle/exportingpdf/ExportingPdfApplication.java
@@ -0,0 +1,13 @@
+package com.tle.exportingpdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportingPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExportingPdfApplication.class, args);
+	}
+
+}
---
+++ exporting-pdf/src/test/java/com/tle/exportingpdf/ExportingPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tle.exportingpdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportingPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
